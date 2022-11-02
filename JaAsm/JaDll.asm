574b11dfa223faed34807690c987a3a79da7b5fb
Konrad Sygut <konrsyg825@student.polsl.pl>
2022-11-03 00:18:37 +0100

    asm in progress

--- JaAsm/JaDll.asm
+++ JaAsm/JaDll.asm
@@ -1,13 +1,31 @@
 .code
-MyProc proc
-mov RAX, RCX
-add RAX, RDX
-ret
-MyProc endp
 
-FloatProc PROC
-addsd xmm0, xmm1
-ret
-FloatProc ENDP
+AsmAlg proc buf: QWORD, part: QWORD, start SDWORD. end SDWORD, factor SDWORD
 
+push r13		;iterator po skladowych piksela czesci bitmapy (iZero)
+push r10		; factor
+push r9			; poczatek czesci bitmapy
+push r8			; koniec czesci bitmapy
+push rcx		; bufor pikseli RGB
+push rdx		; czesc bitmapy
+
+
+xor r13, r13	 ; zerowanie iteratora iZero
+
+
+loop:
+
+cmp r8,r9		; porownanie poczatku z koncem 
+jge endLoop			; jesli  start >= end to skocz do end
+
+
+
+endLoop:
+pop r13
+pop r10
+pop r9
+pop r8 
+pop rcx
+pop rdx
+AsmAlg endp
 end
